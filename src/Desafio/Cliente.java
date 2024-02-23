4ae6658ebe46cab0e4328ffb2b062d267bd30776
renatof4rias <renatof4rias@gmail.com>
2024-02-22 21:57:02 -0300

    Classe Cliente

---
+++ src/Desafio/Cliente.java
@@ -0,0 +1,14 @@
+package Desafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+
+	public String nome;
+	public List<Compra> comprar = new ArrayList<>();
+
+	public Cliente() {
+
+	}
+}
